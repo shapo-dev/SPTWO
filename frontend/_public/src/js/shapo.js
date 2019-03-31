18fedb9d76d63c74ac127e0e6b1c16bc322b700a
Amir Fayaz <amir.fayaz@shapo.de>
2019-03-31 16:18:19 +0200

    Update shapo.js

--- frontend/_public/src/js/shapo.js
+++ frontend/_public/src/js/shapo.js
@@ -163,19 +163,19 @@ $(".backToTop").click(function() {
 // Accordion
 var acc = document.getElementsByClassName("accordion");
 var i;
+console.log(i);
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
-    }
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
   });
 }
-
 
 
 
