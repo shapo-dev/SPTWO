4f46504a27adec42cd937dc09da60981b19c99c3
Amir Fayaz <amir.fayaz@shapo.de>
2019-04-01 13:31:26 +0200

    Update shapo.js

--- frontend/_public/src/js/shapo.js
+++ frontend/_public/src/js/shapo.js
@@ -8,46 +8,12 @@ document.asyncReady(function() {
     // do your magic here  
 
 
-$('.usp--container').slick({
-  centerMode: true,
-  centerPadding: '5px',
-
-  dots: false,
-  infinite: true,
-  speed: 300,
+$('.slick').slick({
   slidesToShow: 3,
-  slidesToScroll: 3,
-  
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
 });
-
 
 
  // Begin init
