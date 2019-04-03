99bc439f709b396dd83a24d30ac01e79b141982a
SHAPO <39989857+shapo-dev@users.noreply.github.com>
2019-04-03 13:18:17 +0200

    Update shapo.js

--- frontend/_public/src/js/shapo.js
+++ frontend/_public/src/js/shapo.js
@@ -7,7 +7,15 @@ console.log("LANGUAGE: " +  lang);
 // ADD SALE CLASS
 $("[title|='SALE']").addClass('sale');
 
-
+// OPEN FIRST 3 FILTERS
+if ( $('body').is('.pretty.awesome') ) {
+  $( ".listing--sidebar .sidebar-filter .filter--container .filter-panel" ).removeClass( "is--collapsed" );
+  $( ".listing--sidebar .sidebar-filter .filter--container .filter-panel:nth-child(1)" ).addClass( "is--collapsed js-added" );
+  $( ".listing--sidebar .sidebar-filter .filter--container .filter-panel:nth-child(2)" ).addClass( "is--collapsed js-added" );
+  $( ".listing--sidebar .sidebar-filter .filter--container .filter-panel:nth-child(3)" ).addClass( "is--collapsed js-added" );
+    //do something it does have the protected class!
+    alert("i have the protected class");
+}
 
 
 console.log('SHAPO ' + 'init')
