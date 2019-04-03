28a915b6689993b9dc82321899b8edce83e82cea
SHAPO <39989857+shapo-dev@users.noreply.github.com>
2019-04-03 14:12:06 +0200

    Update shapo.js

--- frontend/_public/src/js/shapo.js
+++ frontend/_public/src/js/shapo.js
@@ -5,17 +5,7 @@ var langcode = [];
 console.log("LANGUAGE: " +  lang);
 
 
-window.StateManager //Führt JS aus, wenn Viewport wechselt von xs, s, m, l oder xl (Mobile - Desktop)
 
-        // OffCanvas menu
-        .addPlugin('*[data-offcanvas="true"]', 'swOffcanvasMenu', {
-            fullscreen: true,//kann man auch auf false setzen
-        },['xs', 's', 'm', 'l', 'xl'])
-        .addPlugin('*[data-subcategory-nav="true"]', 'swSubCategoryNav', ['xs', 's', 'm', 'l', 'xl'])
-;
-jQuery( window ).load(function( $ ) {
-  
-});
 
 
 // ADD SALE CLASS
