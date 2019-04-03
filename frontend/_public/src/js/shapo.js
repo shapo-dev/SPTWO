5aca57c7d7b5dc96e2c4ce801fe6b55eb66cc080
SHAPO <39989857+shapo-dev@users.noreply.github.com>
2019-04-03 13:09:30 +0200

    Update shapo.js

--- frontend/_public/src/js/shapo.js
+++ frontend/_public/src/js/shapo.js
@@ -1,4 +1,8 @@
 (function($, window) {
+// LANGUAGE
+var lang = $('html').attr('lang');
+var langcode = [];
+console.log("LANGUAGE: " +  lang);
 
 // ADD SALE CLASS
 $("[title|='SALE']").addClass('sale');
