ea7f1c9a40c2653546cc39fc161f2e07115ed396
SHAPO <39989857+shapo-dev@users.noreply.github.com>
2019-04-10 13:39:50 +0200

    Update Theme.php

--- Theme.php
+++ Theme.php
@@ -109,6 +109,88 @@ class Theme extends \Shopware\Components\Theme
     ];
 
 
+  /**
+     * Holds default theme colors.
+     *
+     * @var array
+     */
+    private $themeColorDefaults = [
+        'brand-primary' => '#000000',
+        'brand-primary-light' => 'saturate(lighten(@brand-primary,12%), 5%)',
+        'brand-secondary' => '#5F7285',
+        'brand-secondary-dark' => 'darken(@brand-secondary, 15%)',
+        'gray' => '#F5F5F8',
+        'gray-light' => 'lighten(@gray, 1%)',
+        'gray-dark' => 'darken(@gray-light, 10%)',
+        'border-color' => '@gray-dark',
+        'highlight-success' => '#2ECC71',
+        'highlight-error' => '#E74C3C',
+        'highlight-notice' => '#F1C40F',
+        'highlight-info' => '#4AA3DF',
+        'body-bg' => 'darken(@gray-light, 5%)',
+        'overlay-bg' => '#000000',
+        'overlay-dark-bg' => '@overlay-bg',
+        'overlay-light-bg' => '#FFFFFF',
+        'overlay-opacity' => '0.7',
+        'text-color' => '@brand-secondary',
+        'text-color-dark' => '@brand-secondary-dark',
+        'link-color' => '@brand-primary',
+        'link-hover-color' => 'darken(@link-color, 10%)',
+        'rating-star-color' => '@highlight-notice',
+        'btn-default-top-bg' => '#FFFFFF',
+        'btn-default-bottom-bg' => '@gray-light',
+        'btn-default-hover-bg' => '#FFFFFF',
+        'btn-default-text-color' => '@text-color',
+        'btn-default-hover-text-color' => '@brand-primary',
+        'btn-default-border-color' => '@border-color',
+        'btn-default-hover-border-color' => '@brand-primary',
+        'btn-primary-top-bg' => '@brand-primary-light',
+        'btn-primary-bottom-bg' => '@brand-primary',
+        'btn-primary-hover-bg' => '@brand-primary',
+        'btn-primary-text-color' => '#FFFFFF',
+        'btn-primary-hover-text-color' => '@btn-primary-text-color',
+        'btn-secondary-top-bg' => '@brand-secondary',
+        'btn-secondary-bottom-bg' => '@brand-secondary-dark',
+        'btn-secondary-hover-bg' => '@brand-secondary-dark',
+        'btn-secondary-text-color' => '#FFFFFF',
+        'btn-secondary-hover-text-color' => '@btn-secondary-text-color',
+        'panel-header-bg' => '@gray-light',
+        'panel-header-color' => '@text-color',
+        'panel-border' => '@border-color',
+        'panel-bg' => '#FFFFFF',
+        'label-color' => '@text-color',
+        'input-bg' => '@gray-light',
+        'input-color' => '@brand-secondary',
+        'input-placeholder-color' => 'lighten(@text-color, 15%)',
+        'input-border' => '@border-color',
+        'input-focus-bg' => '#FFFFFF',
+        'input-focus-border' => '@brand-primary',
+        'input-focus-color' => '@brand-secondary',
+        'input-error-bg' => 'desaturate(lighten(@highlight-error, 38%), 20%)',
+        'input-error-border' => '@highlight-error',
+        'input-error-color' => '@highlight-error',
+        'input-success-bg' => '#FFFFFF',
+        'input-success-border' => '@highlight-success',
+        'input-success-color' => '@brand-secondary-dark',
+        'panel-table-header-bg' => '@panel-bg',
+        'panel-table-header-color' => '@text-color-dark',
+        'table-row-bg' => '#FFFFFF',
+        'table-row-color' => '@brand-secondary',
+        'table-row-highlight-bg' => 'darken(@table-row-bg, 4%)',
+        'table-header-bg' => '@brand-secondary',
+        'table-header-color' => '#FFFFFF',
+        'badge-discount-bg' => '@highlight-error',
+        'badge-discount-color' => '#FFFFFF',
+        'badge-newcomer-bg' => '@highlight-notice',
+        'badge-newcomer-color' => '#FFFFFF',
+        'badge-recommendation-bg' => '@highlight-success',
+        'badge-recommendation-color' => '#FFFFFF',
+        'badge-download-bg' => '@highlight-info',
+        'badge-download-color' => '#FFFFFF',
+    ];
+
+
+
 
 
 
