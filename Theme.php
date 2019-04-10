0342d5d9c7ba27ee1fff97d042343accba131ba5
SHAPO <39989857+shapo-dev@users.noreply.github.com>
2019-04-10 14:30:20 +0200

    Update Theme.php

--- Theme.php
+++ Theme.php
@@ -409,7 +409,7 @@ public function createConfig(Form\Container\TabContainer $container)
 
 
 
-    $attributes = array_merge($this->fieldSetDefaults, ['width' => 420]);
+    $attributes = array_merge($this->fieldSetDefaults, ['width' => 700]);
     $topbar_active = $this->createCheckboxField(
         'topbar_active',
         'Topbar aktiv?',
